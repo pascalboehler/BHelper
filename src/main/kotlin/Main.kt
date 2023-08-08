50b64cb88e86c4985109cb4e370f6e0f7df1ee7f
Pascal Böhler <pascal@bembel.dev>
2023-08-08 16:18:38 +0200

    feat: Moved TUI to own class and made functions private

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,8 @@
 fun main() {
-    println("Hello World!")
+    println("BHelper")
 
-    val tui = TUI()
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    while (true) {
-        print("> ")
+    val interactive = true
 
-        //
-        // which commands should be supported:
-        // help: print all available commands
-        // add: add a new entry
-        // create: create a new printout
-        // print / export: same as create
-        // show: show all entries in a table
-        //
-
-        when (val input = readlnOrNull()) {
-            "?" -> tui.help()
-            "help" -> tui.help() // TODO: Create help page
-            "add" -> println("Adding new element")
-            "settings" -> println("Settings page")
-            "clear" -> println("Clearing the interface")
-            "exit" -> break
-            else -> {
-                println("Command not known")
-                tui.help()
-            }
-        }
-    }
+    if (interactive)
+        TUI()
 }
--- src/main/kotlin/TUI.kt
+++ src/main/kotlin/TUI.kt
@@ -1,5 +1,37 @@
 class TUI {
-    fun help() {
+
+    init {
+        println("Starting TUI")
+        start()
+    }
+    private fun start() {
+        while (true) {
+            print("?> ")
+
+            //
+            // which commands should be supported:
+            // help: print all available commands
+            // add: add a new entry
+            // create: create a new printout
+            // print / export: same as create
+            // show: show all entries in a table
+            //
+
+            when (val input = readlnOrNull()) {
+                "?" -> help()
+                "help" -> help() // TODO: Create help page
+                "add" -> println("Adding new element")
+                "settings" -> println("Settings page")
+                "clear" -> println("Clearing the interface")
+                "exit" -> break
+                else -> {
+                    println("Command not known")
+                    help()
+                }
+            }
+        }
+    }
+    private fun help() {
         println("Help")
     }
 }
