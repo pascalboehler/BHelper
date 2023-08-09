aee17466181b229d8d744069e5ebb7d60bbccff1
Pascal Böhler <pascal@bembel.dev>
2023-08-09 18:02:14 +0200

    feat: Added second layer of command options

--- src/TUI/AddEntry.kt
+++ src/TUI/AddEntry.kt
@@ -1,10 +1,17 @@
 package TUI
 
-class AddEntry(private val args: List<String>) {
+class AddEntry(private var args: List<String>) {
     init {
         println("Hi")
         for (arg in args) {
             println(arg)
+        }
+
+        when (args.first()) {
+            "entry" -> println("Adding a new entry")
+            "station" -> println("Adding a new station")
+            "location" -> println("Adding a new location")
+            else -> println("NotImplementedException")
         }
     }
 }
