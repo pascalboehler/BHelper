f2677f1470898557f0bbb49113b0ae8079f6dfd6
Pascal Böhler <pascal@bembel.dev>
2023-08-09 18:48:24 +0200

    chore: Refactored and cleaned up code

--- src/TUI/AddEntry.kt
+++ src/TUI/AddEntry.kt
@@ -1,53 +1,76 @@
 package TUI
 
 class AddEntry(private var args: List<String>) {
+
+    private var isInteractiveMode: Boolean = false
+
     init {
-        isInteractive()
+        addModelSel()
     }
 
-    private fun isInteractive() {
+    private fun addModelSel() {
 
         if (args.isEmpty()) { // MARK: Switch to interactive mode
+            isInteractiveMode = true
             interactiveMode()
             return
         }
+
+        isInteractiveMode = false
 
         when (args.first()) {
             "entry" -> {
                 args = args.subList(1, args.size)
                 createEntryMode()
             }
-            "station" -> println("Adding a new station")
-            "location" -> println("Adding a new location")
+            "station" -> createStationMode()
+            "location" -> createLocationMode()
             else -> println("NotImplementedException")
         }
     }
 
     private fun interactiveMode() {
-        print("Item to create ?> ")
+        print("Item to add ?> ")
 
         val input = readlnOrNull()?.split(" ")
-        val modeSel = input?.first()
-
-        when(modeSel) {
-            "entry" -> createEntryMode()
-            "help" -> help()
+        when(val modeSel = input?.first()) {
             "exit" -> return // MARK: Exit condition for recursive method
             "quit" -> terminate()
-            else -> {
-                println("Argument not found")
-                help()
-            }
+            else -> createModeSel(modeSel)
         }
 
         interactiveMode()
     }
 
+    private fun createModeSel(modeSelArgs: String?) {
+        when(modeSelArgs) {
+            "entry" -> createEntryMode()
+            "station" -> createStationMode()
+            "location" -> createLocationMode()
+            "help" -> addHelp()
+            else -> {
+                println("Argument not found")
+                addHelp()
+            }
+        }
+    }
+
     private fun createEntryMode() {
+        // TODO: Implement add entry function
         println("Creating new entry")
     }
 
-    private fun help() {
+    private fun createStationMode() {
+        // TODO: Implement add station function
+    }
+
+    private fun createLocationMode() {
+        // TODO: Implement add location function
+    }
+
+    private fun addHelp() {
+        // TODO: Implement add help function
+
         println("help me!!")
     }
 }
