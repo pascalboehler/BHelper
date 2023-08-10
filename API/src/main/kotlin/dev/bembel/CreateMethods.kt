d10bd49f3d5fceeb237cadb9be6af9bd4aa73fa7
Pascal Böhler <pascal@bembel.dev>
2023-08-10 15:55:57 +0200

    feat: Deleted startup files and create file for "/create" methdos

---
+++ API/src/main/kotlin/dev/bembel/CreateMethods.kt
@@ -0,0 +1,25 @@
+package dev.bembel
+
+import dev.bembel.utility.apiVersion
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+
+fun Application.configureCreateMethods() {
+    routing {
+        post ("$apiVersion/create/task") {
+
+        }
+
+        post ("$apiVersion/create/station") {
+
+        }
+
+        post ("$apiVersion/create/location") {
+
+        }
+
+        post ("$apiVersion/create/pic") {
+
+        }
+    }
+}
