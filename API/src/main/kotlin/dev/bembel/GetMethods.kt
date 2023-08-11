2fcd7f276a883207264db575bfde36d93a271e8b
Pascal Böhler <pascal@bembel.dev>
2023-08-11 13:40:31 +0200

    feat: Added missing API endpoints to GetMethods

--- API/src/main/kotlin/dev/bembel/GetMethods.kt
+++ API/src/main/kotlin/dev/bembel/GetMethods.kt
@@ -9,7 +9,23 @@ import io.ktor.server.routing.*
 fun Application.configureGetMethods() {
     routing {
         get("$apiVersion/task") {
-            call.respondText("{\"test\": \"it\"}", ContentType.Application.Json)
+            call.respondText("{\"endpoint\": \"Task\"}", ContentType.Application.Json)
+        }
+
+        get("$apiVersion/location") {
+            call.respondText("{\"endpoint\": \"station\"}", ContentType.Application.Json)
+        }
+
+        get("$apiVersion/pic") {
+            call.respondText("{\"endpoint\": \"pic\"}", ContentType.Application.Json)
+        }
+
+        get("$apiVersion/station") {
+            call.respondText("{\"endpoint\": \"station\"}", ContentType.Application.Json)
+        }
+
+        get("$apiVersion/getInfo") {
+            call.respondText("API DOC CAN BE FOUND IN GIT REPO")
         }
     }
 }
